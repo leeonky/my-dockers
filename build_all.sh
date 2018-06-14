f0e02e841b1ea4ba0265eac73530226869585ff2
leeonky <leeonky@gmail.com>
2018-06-14 07:16:18 +0000

    Update build script

--- build_all.sh
+++ build_all.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 set -e
-./build.sh os-base
-./build.sh os-dev
-./build.sh os-vim
-./build.sh rvm
-./build.sh rails-dev
-./build.sh pg
+if ! sudo docker images | grep leeonky/os-base; then
+	./build.sh os-base
+fi
+if ! sudo docker images | grep leeonky/os-dev; then
+	./build.sh os-dev
+fi
+if ! sudo docker images | grep leeonky/vim; then
+	./build.sh vim
+fi
+if ! sudo docker images | grep leeonky/rvm; then
+	./build.sh rvm
+fi
+if ! sudo docker images | grep leeonky/rails-dev; then
+	./build.sh rails-dev
+fi
+if ! sudo docker images | grep leeonky/pg; then
+	./build.sh pg
+fi
 
